ecca4567321a16206d50da49ecaa55ff48325878
Crazelu <eberelucky64@gmail.com>
2021-07-08 07:08:49 +0100

    Makes static string constants private

--- lib/src/sound_manager.dart
+++ lib/src/sound_manager.dart
@@ -3,17 +3,17 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class SoundManager {
-  static final String REQUEST_PERMISSION = "requestPermission";
-  static final String RECORD_AUDIO = "recordAudio";
-  static final String PAUSE_RECORDING = "pauseRecording";
-  static final String RESUMING_RECORDING = "resumeRecording";
-  static final String CANCEL_RECORDING = "cancelRecording";
-  static final String SAVE_RECORDING = "saveRecording";
-  static final String PLAY_AUDIO = "playAudioFile";
-  static final String PAUSE_AUDIO = "pauseAudioFile";
-  static final String STOP_PLAYING_AUDIO = "stopPlayingAudioFile";
-  static final String SEEK_TO = "seekTo";
-  static final String SET_LOOPING = "setLooping";
+  static final String _REQUEST_PERMISSION = "requestPermission";
+  static final String _RECORD_AUDIO = "recordAudio";
+  static final String _PAUSE_RECORDING = "pauseRecording";
+  static final String _RESUMING_RECORDING = "resumeRecording";
+  static final String _CANCEL_RECORDING = "cancelRecording";
+  static final String _SAVE_RECORDING = "saveRecording";
+  static final String _PLAY_AUDIO = "playAudioFile";
+  static final String _PAUSE_AUDIO = "pauseAudioFile";
+  static final String _STOP_PLAYING_AUDIO = "stopPlayingAudioFile";
+  static final String _SEEK_TO = "seekTo";
+  static final String _SET_LOOPING = "setLooping";
 
   static const MethodChannel _channel = const MethodChannel('sound_manager');
 
@@ -22,40 +22,40 @@ class SoundManager {
     return version;
   }
 
-  Future<void> requestPermission() async {
-    return await _channel.invokeMethod(REQUEST_PERMISSION);
+  static Future<void> requestPermission() async {
+    return await _channel.invokeMethod(_REQUEST_PERMISSION);
   }
 
   //sound recording
 
   Future<bool> record({String? fileName, String? filePath}) async {
-    return await _channel.invokeMethod(RECORD_AUDIO);
+    return await _channel.invokeMethod(_RECORD_AUDIO);
   }
 
   Future<void> pauseRecording() async {
-    return await _channel.invokeMethod(PAUSE_RECORDING);
+    return await _channel.invokeMethod(_PAUSE_RECORDING);
   }
 
   Future<void> cancelRecording() async {
-    return await _channel.invokeMethod(CANCEL_RECORDING);
+    return await _channel.invokeMethod(_CANCEL_RECORDING);
   }
 
   Future<void> saveRecording() async {
-    return await _channel.invokeMethod(SAVE_RECORDING);
+    return await _channel.invokeMethod(_SAVE_RECORDING);
   }
 
   //Audio playing
 
   Future<void> play({required String filePath}) async {
     //TODO: Mark filePath as required
-    return await _channel.invokeMethod(PLAY_AUDIO, {"filePath": filePath});
+    return await _channel.invokeMethod(_PLAY_AUDIO, {"filePath": filePath});
   }
 
   Future<void> pause() async {
-    return await _channel.invokeMethod(PAUSE_AUDIO);
+    return await _channel.invokeMethod(_PAUSE_AUDIO);
   }
 
   Future<void> stop() async {
-    return await _channel.invokeMethod(STOP_PLAYING_AUDIO);
+    return await _channel.invokeMethod(_STOP_PLAYING_AUDIO);
   }
 }
